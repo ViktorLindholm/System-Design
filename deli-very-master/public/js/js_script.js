4085d088a0aa8f3c4f74ded22b3fe5d60c51b200
ViktorLindholm <v_lindholm@hotmail.com>
2018-02-09 03:24:20 +0100

    Events

--- deli-very-master/public/js/js_script.js
+++ deli-very-master/public/js/js_script.js
@@ -1,14 +1,43 @@
-function MenuItem(name, kCal, gluten, lactose) {
-  this.name = name;
-  this.kCal = kCal;
-  this.gluten = gluten;
-  this.lactose = lactose;
-  this.getBurgerInfo = function() {
-    return this.name + " " + this.kCal;
-  };
+function MenuItem(name, kCal, lactose, gluten, img) {
+	this.name = name;
+	this.kCal = kCal;
+	this.gluten = gluten;
+	this.lactose = lactose;
+	this.img = img;
 }
 
-var burger = new MenuItem('Hot MF', '180 kcal', 'Contains gluten', 'no lactose');
+var burger0 = new MenuItem('The Fire Burger', '750 kCal', 'Contains lactose', 'Contains gluten','http://bk-apac-prd.s3.amazonaws.com/sites/burgerkingindia.in/files/VegWhopper-Thumb_0_0.png');
+var burger1 = new MenuItem('Fried Turkey Burger', '600 kCal', 'Contains lactose', '', 'http://bk-emea-prd.s3.amazonaws.com/sites/burgerking.co.uk/files/BK_Angus_Burger_300x270px.png');
+var burger2 = new MenuItem('A Double w/Cheese', '1800 kCal', 'Contains lactose', 'Contains gluten', 'http://bk-apac-prd.s3.amazonaws.com/sites/burgerking.co.nz/files/BUR2423D_Kings-Collection_PRODUCT_300x270_02%5B1%5D.png');
+var burgers = [burger0, burger1, burger2];
+var menu = document.getElementById("menu");
 
-console.log(burger.getBurgerInfo());
+for (var burger of burgers)
+{
+	var box = document.createElement('div');
+	var name = document.createTextNode(burger.name);
+	var br = document.createElement('br');
+	var img = document.createElement('img');
+	var title = document.createTextNode(burger.name);
+	var kCal = document.createTextNode(burger.kCal);
+	var gluten = document.createTextNode(burger.gluten);
+	var lactose = document.createTextNode(burger.lactose);
+	var ul = document.createElement('ul');
+	var li0 = document.createElement('li');
+	var li1 = document.createElement('li');
+	var li2 = document.createElement('li');
 
+	box.setAttribute('class','grid-item');
+	menu.appendChild(box);
+	box.appendChild(title);
+	img.setAttribute('src', burger.img);
+	img.setAttribute('class', 'bold');
+	box.appendChild(img);
+	box.appendChild(ul);
+	ul.appendChild(li0);
+	ul.appendChild(li1);
+	ul.appendChild(li2);
+	li0.appendChild(kCal);
+	li1.appendChild(gluten);
+	li2.appendChild(lactose);
+}
---
+++ deli-very-master/public/js/menu.js
@@ -0,0 +1,24 @@
+var burgers = [
+{
+	"name": "The Fire Burger",
+	"kCal": 750,
+	"lactose": true,
+	"gluten": true,    
+	"img": "http://bk-apac-prd.s3.amazonaws.com/sites/burgerkingindia.in/files/VegWhopper-Thumb_0_0.png"
+},
+
+{
+	"name": "Fried Turkey Burger",
+	"kCal": 600,
+	"lactose": true,
+	"gluten": false,    
+	"img": "http://bk-emea-prd.s3.amazonaws.com/sites/burgerking.co.uk/files/BK_Angus_Burger_300x270px.png"
+},
+
+"name": "A Double w/Cheese",
+"kCal": 1800,
+"lactose": true,
+"gluten": true,    
+"img": "http://bk-apac-prd.s3.amazonaws.com/sites/burgerking.co.nz/files/BUR2423D_Kings-Collection_PRODUCT_300x270_02%5B1%5D.png"
+}
+]
